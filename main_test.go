b07fab1843751a74f89643dfa7a2ca5f81cc6054
Gabriel Olivério <gabrieloliverio.py@gmail>
2024-12-08 16:35:15 -0300

    Refactor findReplace and add tests for group capturing as replacement

--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestFindReplace(t *testing.T) {
+func TestFindReplace_ReplaceAsSimpleString(t *testing.T) {
 	searchPattern := regexp.MustCompile("text")
 	replace := "replacement"
 	subject := "this is some text, this is some other text"
@@ -16,8 +16,21 @@ func TestFindReplace(t *testing.T) {
 	if !want.MatchString(result) {
 		t.Fatalf(`search() = %q, want %#q`, result, want)
 	}
-
 }
+
+func TestFindReplace_CapturingGroup(t *testing.T) {
+	searchPattern := regexp.MustCompile("(text)")
+	replace := "other $1"
+	subject := "this is some text"
+	result := findReplace(searchPattern, replace, subject)
+
+	want := regexp.MustCompile("this is some other text")
+
+	if !want.MatchString(result) {
+		t.Fatalf(`search() = %q, want %#q`, result, want)
+	}
+}
+
 func TestFindReplaceNotMatch(t *testing.T) {
 	searchPattern := regexp.MustCompile("<fooo>")
 	replace := "replacement"
