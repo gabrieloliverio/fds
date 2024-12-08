197dbffb3f3dd46c8a844624beb993ccf783f5b1
Gabriel Olivério <gabrieloliverio.py@gmail>
2024-12-08 12:52:32 -0300

    Get subject from positional argument

--- main.go
+++ main.go
@@ -9,52 +9,66 @@ import (
 )
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    var searchArg = flag.Arg(0)
-    var replace = flag.Arg(1)
-    var subject = readSubject(os.Stdin)
+	var subject, searchArg, replace string
+	subject = readSubject(os.Stdin)
 
-    searchPattern, err := validateArgs(searchArg, replace, subject)
+	if subject != "" {
+		searchArg = flag.Arg(0)
+		replace = flag.Arg(1)
+	} else {
+		subject = flag.Arg(0)
+		searchArg = flag.Arg(1)
+		replace = flag.Arg(2)
+	}
 
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    
-    result := findReplace(searchPattern, replace, subject)
+	searchPattern, err := validateArgs(searchArg, replace, subject)
 
-    fmt.Println(result)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	result := findReplace(searchPattern, replace, subject)
+
+	fmt.Println(result)
 }
 
 func readSubject(stdin *os.File) string {
-    scanner := bufio.NewScanner(stdin)
-    subject := ""
+	stat, _ := stdin.Stat()
 
-    for scanner.Scan() {
-        subject += scanner.Text()
-    }
+	if stat.Size() == 0 {
+		return ""
+	}
 
-    return subject
+	scanner := bufio.NewScanner(stdin)
+	subject := ""
+
+	for scanner.Scan() {
+		subject += scanner.Text()
+	}
+
+	return subject
 }
 
 func validateArgs(search, replace, subject string) (*regexp.Regexp, error) {
-    searchPattern, err := regexp.Compile(search)
-    if err != nil {
-        err = fmt.Errorf("search is not a valid pattern")
-    }
+	searchPattern, err := regexp.Compile(search)
+	if err != nil {
+		err = fmt.Errorf("search is not a valid pattern")
+	}
 
-    if replace == "" {
-        err = fmt.Errorf("replace is empty")
-    }
+	if replace == "" {
+		err = fmt.Errorf("replace is empty")
+	}
 
-    if subject == "" {
-        err = fmt.Errorf("subject is empty")
-    }
+	if subject == "" {
+		err = fmt.Errorf("subject is empty")
+	}
 
-    return searchPattern, err
+	return searchPattern, err
 }
 
 func findReplace(searchPattern *regexp.Regexp, replace, subject string) string {
-    return string(searchPattern.ReplaceAll([]byte(subject), []byte(replace)))
+	return string(searchPattern.ReplaceAll([]byte(subject), []byte(replace)))
 }
